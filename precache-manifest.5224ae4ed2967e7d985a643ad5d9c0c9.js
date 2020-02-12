b5bd60276e677a88c926212b3f592765ed65f050
harshdave1041995 <harshdave1094@gmail.com>
2020-02-12 18:14:07 +0530

    36f00c1

---
+++ precache-manifest.5224ae4ed2967e7d985a643ad5d9c0c9.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f44a256ba66930b9757f",
+    "url": "/css/app.649a6e12.css"
+  },
+  {
+    "revision": "4730f6a48ab73a3df880",
+    "url": "/css/chunk-vendors.1c425fdd.css"
+  },
+  {
+    "revision": "1ea38e9a1a8e947137b22fd971a42d37",
+    "url": "/fonts/CalibreBlack.1ea38e9a.woff"
+  },
+  {
+    "revision": "1d5dd79a69d90dc005d3afdc6d193e8b",
+    "url": "/fonts/CalibreBlackItalic.1d5dd79a.woff"
+  },
+  {
+    "revision": "b99fc08db548d351a378861a05d76b1c",
+    "url": "/fonts/CalibreBold.b99fc08d.woff"
+  },
+  {
+    "revision": "c9ffe690df68ed5351b31c0c5c6598ca",
+    "url": "/fonts/CalibreBoldItalic.c9ffe690.woff"
+  },
+  {
+    "revision": "ed571050858663f68ef4e33177829044",
+    "url": "/fonts/CalibreLight.ed571050.woff"
+  },
+  {
+    "revision": "cd88d6a735da2129295a7f50de7decda",
+    "url": "/fonts/CalibreLightItalic.cd88d6a7.woff"
+  },
+  {
+    "revision": "48619aaa669be0b954bfb06907dce32d",
+    "url": "/fonts/CalibreMedium.48619aaa.woff"
+  },
+  {
+    "revision": "7e9ed3a0af12a512911da34fe8a73b4f",
+    "url": "/fonts/CalibreMediumItalic.7e9ed3a0.woff"
+  },
+  {
+    "revision": "c5b01b24488621f886d64db77dd676a2",
+    "url": "/fonts/CalibreRegular.c5b01b24.woff"
+  },
+  {
+    "revision": "88905342188fcd71e312bdcebef295fc",
+    "url": "/fonts/CalibreRegularItalic.88905342.woff"
+  },
+  {
+    "revision": "0cf4497b765aa0466ead3d66f188835f",
+    "url": "/fonts/CalibreSemibold.0cf4497b.woff"
+  },
+  {
+    "revision": "9ddd82ffff8a4dcdc788b7fb6d95ea0d",
+    "url": "/fonts/CalibreSemiboldItalic.9ddd82ff.woff"
+  },
+  {
+    "revision": "d262219bcb9b5de8cf763933b4eefb1d",
+    "url": "/fonts/CalibreThin.d262219b.woff"
+  },
+  {
+    "revision": "e0c4ddccd87546b50b91783212e4d131",
+    "url": "/fonts/CalibreThinItalic.e0c4ddcc.woff"
+  },
+  {
+    "revision": "f41ae5943e63ad5402fba408d1c06fcb",
+    "url": "/img/close.f41ae594.svg"
+  },
+  {
+    "revision": "4b7afea1a7ac86d48bf007d6e9eeece1",
+    "url": "/img/info-pic.4b7afea1.png"
+  },
+  {
+    "revision": "cef58d902411576b37805e096e76e634",
+    "url": "/img/menu.cef58d90.svg"
+  },
+  {
+    "revision": "73ba51a06474a4c4e48c76d16d2f03e0",
+    "url": "/img/website-logo_burned.73ba51a0.png"
+  },
+  {
+    "revision": "8de18a601ac75e0da579f67ed7104cda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f44a256ba66930b9757f",
+    "url": "/js/app.5264b888.js"
+  },
+  {
+    "revision": "4730f6a48ab73a3df880",
+    "url": "/js/chunk-vendors.953db70a.js"
+  },
+  {
+    "revision": "017b5d3dd8ae66da3519f8cae5b06931",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
